f3fcad11058d67f8ba8335f79ff4165be55630c5
tom mise <kimise@espe.edu.ec>
2024-04-12 20:52:56 -0500

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+import Home from '../src/pages/home';
+import About from './About';
+import Contact from './Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <nav>
+            <Link to="/">Home</Link>{' '}
+            <Link to="/about">About</Link>{' '}
+            <Link to="/contact">Contact</Link>
+          </nav>
+        </header>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div style={styles.container}>
+      <h1>KAIZEN SOFTWARE</h1>
+      <p>Tú visión nuestro</p>
+      <p>Futuro</p>
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    marginTop: 50,
+  },
+};
+
+export default Home;
